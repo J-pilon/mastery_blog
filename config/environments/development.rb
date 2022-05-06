aa756ec74bd848a0bf5f7e9070904782e1f92fb7
Josiah <josiah.pilon@gmail.com>
2022-05-06 12:22:35 -0700

    Removes sass

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,8 +59,8 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
-  config.sass.inline_source_maps = true
-  config.sass.line_comments = false
+  # config.sass.inline_source_maps = true
+  # config.sass.line_comments = false
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
