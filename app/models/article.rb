ca75958bb32c46c5642d7de62ced555d4617be25
Josiah <josiah.pilon@gmail.com>
2022-05-18 12:24:39 -0700

    Adds private method that sanitizes the body

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,10 @@
 class Article < ApplicationRecord
+    require 'sanitize'
+
     validates :title, presence: true
     validates :body, presence: true
 
+    before_validation :sanitize_body
     after_validation :set_slug
 
     def to_param
@@ -16,5 +19,9 @@ class Article < ApplicationRecord
 
     def set_slug
         self.slug = title.to_s.parameterize
+    end
+
+    def sanitize_body
+        self.body = Sanitize.fragment(body, Sanitize::Config::RELAXED)
     end
 end
