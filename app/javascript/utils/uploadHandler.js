3c3af8ad9f41f970068147f35543c7acdf42f4ed
Josiah <josiah.pilon@gmail.com>
2022-12-14 18:59:48 -0800

    feat: uploadHandler controller

--- app/javascript/utils/uploadHandler.js
+++ app/javascript/utils/uploadHandler.js
@@ -2,7 +2,7 @@ export async function imagesUploadHandler(blobInfo, success, failure, progress) 
     // fetch presigned url for direct browser upload to S3
     const res = await fetch('/uploads/presigned_url', {method: 'GET'});
     const { presigned_url, download_url } = await res.json();
-
+    
     // upload the file to S3 using presigned url
     try {
         const file = blobInfo.blob()
@@ -11,4 +11,5 @@ export async function imagesUploadHandler(blobInfo, success, failure, progress) 
     } catch (err) {
         failure(`Image upload failed: ${err.message}`)
     }
+    return download_url
 }
