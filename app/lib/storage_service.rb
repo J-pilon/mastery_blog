9fa2be0bdd714ea6d68935b4a84bb95a72f01aa1
Josiah <josiah.pilon@gmail.com>
2023-03-10 15:52:26 -0800

    refact: uses bucket object to get presigned url

--- app/lib/storage_service.rb
+++ app/lib/storage_service.rb
@@ -1,36 +1,32 @@
 class StorageService
   require 'securerandom'
+  
+  attr_reader :bucket_name, :object_key, :bucket
 
   REGION = ENV["AWS_REGION"]
   ACCESS_KEY_ID = ENV["AWS_ACCESS_KEY_ID"]
   SECRET_ACCESS_KEY = ENV["AWS_SECRET_ACCESS_KEY"]
 
-  def initialize(client: init_s3_client, filename: SecureRandom.hex(10))
-    @client = client
-    @filename = filename
+  def initialize(bucket_name:, bucket:, object_key: default_key)
+    @bucket_name = bucket_name
+    @bucket = bucket
+    @object_key = object_key
   end
 
   def download_url
-    "https://#{bucket_name}.s3.#{REGION}.amazonaws.com/#{@filename}"
+    "https://#{bucket_name}.s3.#{REGION}.amazonaws.com/#{object_key}"
   end
 
   def presigned_url
-    presigner = Aws::S3::Presigner.new(client: @client)
-    url, _ = presigner.presigned_request(:put_object, bucket: bucket_name, key: @filename)
-    url
+    url = bucket.object(object_key).presigned_url(:put)
+    puts "Created presigned URL: #{url}"
+    return URI(url)
+
+  rescue Aws::Errors::ServiceError => e
+    puts "Couldn't create presigned URL for #{bucket.name}:#{object_key}. Here's why: #{e.message}"
   end
 
-  private
-
-  def init_s3_client
-    Aws::S3::Client.new(
-      region: REGION,
-      access_key_id: ACCESS_KEY_ID,
-      secret_access_key: SECRET_ACCESS_KEY
-    )
-  end
-
-  def bucket_name
-    ENV["RAILS_ENV"] == "production" ? ENV["AWS_BUCKET_PROD"] : ENV["AWS_BUCKET_DEV"]
+  def default_key
+    SecureRandom.hex(10)
   end
 end
