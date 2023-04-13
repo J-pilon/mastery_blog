86d65c2e090f8e17ec03f96c20fddff997403e9d
Josiah <josiah.pilon@gmail.com>
2023-04-12 19:18:47 -0700

    db: update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_13_213128) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_13_015942) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,11 +23,21 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_13_213128) do
     t.string "image_url"
   end
 
+  create_table "profiles", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "profile_id"
+    t.index ["profile_id"], name: "index_users_on_profile_id"
   end
 
+  add_foreign_key "users", "profiles"
 end
