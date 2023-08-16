300f2a4fa9f46b0a98625fc7624db7819561f087
Josiah <josiah.pilon@gmail.com>
2023-08-16 16:53:19 -0700

    feat: adds routes for sessions and registering

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   root "homepage#index"
   
-  get 'users/signup', to: 'users#new'
-  post 'users/create'
+  resources :registrations, only: :create
+  get '/signup', to: 'registrations#new'
+  get '/signin', to: 'sessions#new'
+  post '/sessions', to: 'sessions#create'
+  post '/signout', to: 'sessions#destroy'
 
   get 'uploads/presigned_url'
 
