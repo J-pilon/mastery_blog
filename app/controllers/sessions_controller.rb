30fbe9037c958a5ab074a9b84d07c63cafa38ee4
Josiah <josiah.pilon@gmail.com>
2023-08-16 16:52:17 -0700

    feat: adds SessionsController with crud actions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by(email: session_params[:email])
+    if @user && @user.authenticate(session_params[:password])
+      session[:user_id] = @user.id
+      redirect_to articles_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to signin_path
+  end
+
+  private
+
+  def session_params
+    params.permit(:email, :password)
+  end
+end
