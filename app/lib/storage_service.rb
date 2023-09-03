7a00e885f0e95d9b34d6239279e40c12b64eb02c
Josiah <josiah.pilon@gmail.com>
2023-09-02 21:53:30 -0700

    refact: adds object instance variable

--- app/lib/storage_service.rb
+++ app/lib/storage_service.rb
@@ -1,32 +1,32 @@
 class StorageService
   require 'securerandom'
   
-  attr_reader :bucket_name, :object_key, :bucket
+  attr_reader :bucket, :object, :object_key
 
-  REGION = ENV["AWS_REGION"]
-  ACCESS_KEY_ID = ENV["AWS_ACCESS_KEY_ID"]
-  SECRET_ACCESS_KEY = ENV["AWS_SECRET_ACCESS_KEY"]
-
-  def initialize(bucket_name:, bucket:, object_key: default_key)
-    @bucket_name = bucket_name
+  def initialize(bucket:, object_key: default_object_key)
     @bucket = bucket
     @object_key = object_key
+    @object = create_object
   end
 
   def download_url
-    "https://#{bucket_name}.s3.#{REGION}.amazonaws.com/#{object_key}"
+    return if !object
+    object.public_url
   end
 
   def presigned_url
-    url = bucket.object(object_key).presigned_url(:put)
-    puts "Created presigned URL: #{url}"
-    return URI(url)
-
-  rescue Aws::Errors::ServiceError => e
-    puts "Couldn't create presigned URL for #{bucket.name}:#{object_key}. Here's why: #{e.message}"
+    return if !object
+    object.presigned_url(:put)
   end
 
-  def default_key
+  private
+
+  def create_object
+    return if !bucket
+    bucket.object(object_key)
+  end
+  
+  def default_object_key
     SecureRandom.hex(10)
   end
 end
