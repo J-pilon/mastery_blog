40e8e0a9eda7f7d95bbcadb52e70de62479e0c68
Josiah <josiah.pilon@gmail.com>
2023-09-04 14:44:08 -0700

    refact: adds method that instantiates Aws::S3::Bucket

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,16 +1,6 @@
 class UploadsController < ApplicationController
   def storage_service_urls
-    storage = StorageService.new(bucket: aws_bucket)
+    storage = StorageService.new
     render json: { presigned_url: storage.presigned_url, download_url: storage.download_url }
-  end
-
-  private
-  
-  def aws_bucket
-    Aws::S3::Bucket.new(aws_bucket_name)
-  end
-
-  def aws_bucket_name
-    ENV["RAILS_ENV"] == "production" ? ENV["AWS_BUCKET_PROD"] : ENV["AWS_BUCKET_DEV"]
   end
 end
