b71ed5d215fa2a2b1d58b70a72dbe7165f3e2f51
Josiah <josiah.pilon@gmail.com>
2023-09-13 20:39:23 -0700

    refact: move aws_bucket_name into StorageService

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,6 +1,13 @@
 class UploadsController < ApplicationController
   def storage_service
-    storage = StorageService.new
+    storage = StorageService.new(bucket: Aws::S3::Bucket.new(aws_bucket_name))
     render json: { presigned_url: storage.presigned_url, download_url: storage.download_url }
+  end
+
+  private
+
+  def aws_bucket_name
+    return ENV["AWS_BUCKET_DEV"] if ENV["RAILS_ENV"] == "development"
+    return "test" if ENV["RAILS_ENV"] == "test"
   end
 end
