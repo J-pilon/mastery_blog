ccf1ea0110320fa64333fff0d51f333f880e9b6d
Josiah <josiah.pilon@gmail.com>
2023-09-14 14:04:56 -0700

    test: expect post /signup responds with status 302

--- spec/requests/registrations_spec.rb
+++ spec/requests/registrations_spec.rb
@@ -7,4 +7,13 @@ RSpec.describe "Registrations", type: :request do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe "POST /signup" do
+    it "responds with status 302" do
+      user_params = FactoryBot.attributes_for(:user)
+      profile_params = FactoryBot.attributes_for(:profile)
+      post signup_path, params: {user: user_params, profile: profile_params}
+      expect(response).to have_http_status(302)
+    end
+  end
 end
