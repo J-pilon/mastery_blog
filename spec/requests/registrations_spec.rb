646e134d3fa9a57f8b603c467c9b9e5666ed3b13
Josiah <josiah.pilon@gmail.com>
2023-09-14 14:13:05 -0700

    test: post /signup with invalid params responds with 422

--- spec/requests/registrations_spec.rb
+++ spec/requests/registrations_spec.rb
@@ -9,11 +9,22 @@ RSpec.describe "Registrations", type: :request do
   end
 
   describe "POST /signup" do
-    it "responds with status 302" do
-      user_params = FactoryBot.attributes_for(:user)
-      profile_params = FactoryBot.attributes_for(:profile)
-      post signup_path, params: {user: user_params, profile: profile_params}
-      expect(response).to have_http_status(302)
+    context 'when params are valid' do
+      it "responds with status 302" do
+        user_params = FactoryBot.attributes_for(:user)
+        profile_params = FactoryBot.attributes_for(:profile)
+        post signup_path, params: {user: user_params, profile: profile_params}
+        expect(response).to have_http_status(302)
+      end
+    end
+
+    context 'when params are invalid' do
+      it "responds with status 422" do
+        user_params = FactoryBot.attributes_for(:user, email: nil)
+        profile_params = FactoryBot.attributes_for(:profile)
+        post signup_path, params: {user: user_params, profile: profile_params}
+        expect(response).to have_http_status(422)
+      end
     end
   end
 end
