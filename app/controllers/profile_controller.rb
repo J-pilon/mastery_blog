6c2c62396237bb74790099adc4add5e2b0060fc1
Josiah <josiah.pilon@gmail.com>
2023-09-15 19:10:17 -0700

    feat: create profile controller with #show, #edit, #update

---
+++ app/controllers/profile_controller.rb
@@ -0,0 +1,25 @@
+class ProfileController < ApplicationController
+  before_action :profile
+  
+  def show; end
+
+  def edit; end
+
+  def update
+    if @profile.update(profile_params)
+      redirect_to profile_path(id: @profile)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def profile
+    @profile = Profile.find(params[:id])    
+  end
+
+  def profile_params
+    params.require(:profile).permit(:first_name, :last_name, :image_url)
+  end
+end
