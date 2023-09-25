5cf6000174704e84e0d894e66ed288c5997f6c12
Josiah <josiah.pilon@gmail.com>
2023-09-24 19:15:05 -0700

    feat: adds generate_reset_token to model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :email, presence: true, uniqueness: true
+
+  def generate_reset_token
+    token = SecureRandom.uuid
+    self.reset_token = token
+    self.reset_token_expiry = DateTime.now + 30.minutes
+  end
 end
