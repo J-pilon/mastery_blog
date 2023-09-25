6dd9392c539fcf6013d6c3e600d6e32f9454f758
Josiah <josiah.pilon@gmail.com>
2023-09-24 19:16:28 -0700

    db: update schema after AddResetTokenToUsers migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_19_203508) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_25_020248) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -39,6 +39,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_19_203508) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "profile_id"
+    t.string "reset_token"
+    t.datetime "reset_token_expiry"
     t.index ["profile_id"], name: "index_users_on_profile_id"
   end
 
