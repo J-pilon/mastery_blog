8bc06021cb294da06fc83cb4de13fe2d558826f2
Josiah <josiah.pilon@gmail.com>
2023-09-25 22:01:08 -0700

    feat: adds routes :new, :create, :edit, :update for users/password

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,9 @@ Rails.application.routes.draw do
 
   resource :profile, only: [:show, :edit, :update], controller: 'profile'
 
-  resource :user, only: [:edit, :update], controller: 'user'
+  resource :users do
+    resource :password, only: [:new, :create, :edit, :update], controller: 'password'
+  end
   
   resources :articles
 end
