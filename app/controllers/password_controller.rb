68e208b9301a90c45cc545c33dd6656b310b319f
Josiah <josiah.pilon@gmail.com>
2023-09-29 12:48:58 -0700

    feat: adds aws_email_service.send_email!

--- app/controllers/password_controller.rb
+++ app/controllers/password_controller.rb
@@ -1,12 +1,12 @@
 class PasswordController < ApplicationController
+  before_action :find_user_by_email, only: [:create, :update]
+
   def new
     @user = User.new
   end
 
   def create
-    @user = User.find_by(email: params[:email])
-
-    if @user && @user.generate_reset_token
+    if @user && @user.generate_reset_token && aws_email_service.send_email!
       redirect_to email_sent_users_password_path
     else
       render :new, status: :unprocessable_entity
@@ -32,7 +32,24 @@ class PasswordController < ApplicationController
 
   private
 
+  def find_user_by_email
+    @user = User.find_by(email: params[:user][:email])
+  end
+
   def password_params
-    params.require(:user).permit(:password, :password_confirmation)
+    params.require(:user).permit(:password)
+  end
+
+  def aws_email_service
+    edit_users_password_link = edit_users_password_path(reset_token: @user.reset_token)
+
+    EmailService.new(client: aws_ses_client, 
+                      users: @user, 
+                      type: "password_reset_email",
+                      link: edit_users_password_link)
+  end
+
+  def aws_ses_client
+    Aws::SES::Client.new
   end
 end
