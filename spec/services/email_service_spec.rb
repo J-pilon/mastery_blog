56d387445648eab054dce53e8e1c019a0d8546c5
Josiah <josiah.pilon@gmail.com>
2023-09-29 12:52:55 -0700

    test: email service for aws ses

---
+++ spec/services/email_service_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe "EmailService" do
+  let(:users) { FactoryBot.create_list(:user, 3) }
+  let(:client) { Aws::SES::Client.new(region: "awsregion", stub_responses: true) }
+  let(:email_type) { "password_reset_email" }
+  let(:valid_email_service) do 
+    EmailService.new(client: client, 
+                      users: users, 
+                      type: email_type)
+  end
+
+  it "responds to send!" do 
+    expect(valid_email_service.respond_to?(:send_email!)).to be(true)
+  end
+
+  context 'when valid' do
+    it "returns responds with a message id" do
+      expect(valid_email_service.send_email!).to eq({message_id: "MessageId"})
+    end
+  end
+end
