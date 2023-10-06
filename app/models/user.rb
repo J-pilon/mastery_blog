070c10fce18c0afe52bb15080e4a6d78f900ffab
Josiah <josiah.pilon@gmail.com>
2023-10-06 13:34:54 -0700

    refact: extracts token and token_time_limit to methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,21 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
 
   def generate_reset_token
-    token = SecureRandom.uuid
-    update(reset_token_expiry: DateTime.now + 30.minutes, reset_token: token)
+    update(reset_token_expiry: token_time_limit, reset_token: token)
   end
 
-  def is_reset_token_valid?(token)
-    DateTime.now < reset_token_expiry && reset_token == token
+  def is_reset_token_valid?
+    DateTime.now < reset_token_expiry
+    # should the token be reset after checking
+  end
+
+  private
+
+  def token
+    SecureRandom.uuid
+  end
+
+  def token_time_limit
+    DateTime.now + 5.minutes
   end
 end
