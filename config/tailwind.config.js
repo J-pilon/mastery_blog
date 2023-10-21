8589187b6bf6819c4cb3b7276d55d2eff44fe18e
Josiah <josiah.pilon@gmail.com>
2023-10-21 15:02:39 -0700

    chore: adds colors

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -6,10 +6,23 @@ module.exports = {
     './app/javascript/**/*.js',
     './app/views/**/*.{erb,haml,html,slim}'
   ],
+  safelist: [
+    // 'bg-background',
+    // 'bg-red-800',
+  ],
   theme: {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        arimo: "Arimo"
+      },
+      colors: {
+        'light': '#FFFAF4',
+        'black': '#1A1B1F',
+        'primary': '#41729F',
+        'secondary': '#C3E0E5',
+        'tertiary': '#274472',
+        'background': '#5885AF',
       },
     },
   },
