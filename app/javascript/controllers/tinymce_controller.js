9c595cbaabe5eddb120e51e5dd33855c9f6fa52d
Josiah <josiah.pilon@gmail.com>
2023-10-29 19:08:32 -0700

    chore: tinymce config

--- app/javascript/controllers/tinymce_controller.js
+++ app/javascript/controllers/tinymce_controller.js
@@ -11,10 +11,9 @@ export default class extends Controller {
         //   "searchreplace visualblocks code fullscreen",
         //   "insertdatetime media table paste code help wordcount",
         // ],
-        toolbar:
-          "file edit view insert format tools undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help | image",
-        content_style: "body {font-size: 11pt; color: #374151} img {width: 50%; height: auto}",
-        skin: "oxide-dark",
+        toolbar: "file edit view insert format tools undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help | image",
+        // skin: "oxide-dark",
+        content_css: "/assets/tinymce",
         automatic_uploads: true,
         images_upload_handler: imagesUploadHandler
       });
