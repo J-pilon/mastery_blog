fd941b313cfdaa94f588439292309db76e17a267
Josiah <josiah.pilon@gmail.com>
2023-11-02 13:50:23 -0700

    chore: increase article body's length

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :article do
-    title { LiterateRandomizer.sentence(:words => 5) }
-    body { LiterateRandomizer.paragraph(:sentences => 8) }
+    title { LiterateRandomizer.sentence(words: 5) }
+    body { LiterateRandomizer.paragraphs(paragraphs: 3, sentences: 10) }
   end
 end
