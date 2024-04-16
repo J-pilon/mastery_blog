c4b79e2a3301836f8a0aa5bcb23c336d9a105fb9
Josiah <josiah.pilon@gmail.com>
2024-04-15 20:50:46 -0700

    chore: clean up

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,8 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(email: session_params[:email])
-    if @user && @user.authenticate(session_params[:password])
+
+    if validate_email && validate_password
       session[:user_id] = @user.id
       redirect_to articles_path
     else
@@ -21,6 +22,26 @@ class SessionsController < ApplicationController
   private
 
   def session_params
-    params.permit(:email, :password)
+    params.require(:user).permit(:email, :password)
+  end
+
+  def validate_password
+    unless @user.authenticate(session_params[:password])
+      @user = User.new
+      @user.errors.add(:password,  'is incorrect')
+      return false
+    end
+
+    return true
+  end
+
+  def validate_email
+    unless @user
+      @user = User.new
+      @user.errors.add(:email, 'not found. Please sign up first.')
+      return false
+    end
+
+    return true
   end
 end
