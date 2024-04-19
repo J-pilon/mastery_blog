8ed88c903a744e40d1046352e3aac5d75d2bd0d3
Josiah <josiah.pilon@gmail.com>
2024-04-18 21:04:04 -0700

    fix: adds event listener for download url

--- app/javascript/controllers/tinymce_controller.js
+++ app/javascript/controllers/tinymce_controller.js
@@ -1,25 +1,41 @@
-import { Controller } from "@hotwired/stimulus"
-import { imagesUploadHandler } from "../utils/uploadHandler"
+import { Controller } from "@hotwired/stimulus";
+import { imagesUploadHandler } from "../utils/uploadHandler";
 
 export default class extends Controller {
   connect() {
     tinymce.init({
-        selector: "textarea.tinymce",
-        // plugins: [
-        //   "image",
-        //   "advlist autolink lists link image charmap print preview anchor",
-        //   "searchreplace visualblocks code fullscreen",
-        //   "insertdatetime media table paste code help wordcount",
-        // ],
-        toolbar: "file edit view insert format tools undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help | image",
-        // skin: "oxide-dark",
-        content_css: "/assets/tinymce",
-        automatic_uploads: true,
-        images_upload_handler: imagesUploadHandler
-      });
+      selector: "textarea#tinymce",
+      height: 800,
+      // plugins: [
+      //   "image",
+      //   "advlist autolink lists link image charmap print preview anchor",
+      //   "searchreplace visualblocks code fullscreen",
+      //   "insertdatetime media table paste code help wordcount",
+      // ],
+      toolbar:
+        "file edit view insert format tools undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help | image",
+      // skin: "oxide-dark",
+      content_css: "/assets/tinymce",
+      automatic_uploads: true,
+      images_upload_handler: imagesUploadHandler,
+    });
+
+    document.addEventListener("imageUploadSuccess", this.onImageUploadSuccess);
   }
-  
+
+  onImageUploadSuccess(e) {
+    const downloadUrl = e.detail.downloadUrl;
+    const imageElement = document.querySelector("input#article_image_url");
+    imageElement.value = downloadUrl;
+    console.log(imageElement.value, " - Image URL set.");
+  }
+
   disconnect() {
-    tinymce.activeEditor.destroy()
+    // console.log("Tinymce controller disconnected.");
+    document.removeEventListener(
+      "imageUploadSuccess",
+      this.onImageUploadSuccess
+    );
+    tinymce.activeEditor.destroy();
   }
 }
--- app/javascript/utils/uploadHandler.js
+++ app/javascript/utils/uploadHandler.js
@@ -1,24 +1,51 @@
-export function imagesUploadHandler(blob, _) {return new Promise(async (success, failure) => {
-    const storageServiceUrl = '/uploads/storage_service'
-    let file = blob
+export function imagesUploadHandler(blob, _) {
+  return new Promise(async (success, failure) => {
+    const storageServiceUrl = "/uploads/storage_service";
+    let file = blob;
+    let storageServiceRes, presigned_url, download_url;
 
-    if (!(file instanceof(File)) || !(file instanceof(Blob))) {
-        file = blob.blob()
+    if (!(file instanceof File) || !(file instanceof Blob)) {
+      file = blob.blob();
     }
 
     try {
-        const responseFromStorageService = await fetch(storageServiceUrl, {method: 'GET'})
-        if(!responseFromStorageService.ok) {
-            throw `${responseFromStorageService.status} - ${responseFromStorageService.statusText}`
-        }
-        const { presigned_url, download_url } = await responseFromStorageService.json()
+      storageServiceRes = await fetch(storageServiceUrl, {
+        method: "GET",
+      });
 
-        const responseFromUpload = await fetch(presigned_url, {method: 'PUT', body: file})
-        if(!responseFromUpload.ok) {
-          throw `${responseFromUpload.status} - ${responseFromUpload.statusText}`
-        }
-        success(download_url)
+      if (storageServiceRes.ok === false) {
+        throw `${storageServiceRes.status} - ${storageServiceRes.statusText} - while fetching the storage service URL.`;
+      }
     } catch (err) {
-        failure({message: err, remove: true})
+      failure({ message: err, remove: true });
     }
-})}
+
+    try {
+      const urls = await storageServiceRes.json();
+      presigned_url = urls.presigned_url;
+      download_url = urls.download_url;
+    } catch (err) {
+      failure({ message: err, remove: true });
+    }
+
+    try {
+      const uploadRes = await fetch(presigned_url, {
+        method: "PUT",
+        body: file,
+      });
+
+      if (!uploadRes.ok) {
+        throw `${uploadRes.status} - ${uploadRes.statusText}`;
+      }
+
+      const event = new CustomEvent("imageUploadSuccess", {
+        detail: { downloadUrl: download_url },
+      });
+      document.dispatchEvent(event);
+
+      success(download_url);
+    } catch (err) {
+      failure({ message: err, remove: true });
+    }
+  });
+}
