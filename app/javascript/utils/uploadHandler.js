d28315eb6a8ebf0d828a77ec18aa8907c0856684
Josiah <josiah.pilon@gmail.com>
2024-04-19 22:26:05 -0700

    refact: imageUploadHander

--- app/javascript/utils/uploadHandler.js
+++ app/javascript/utils/uploadHandler.js
@@ -1,47 +1,15 @@
-export function imagesUploadHandler(blob, _) {
+export function tinymceImageUploadHandler(blob, _) {
   return new Promise(async (success, failure) => {
     const storageServiceUrl = "/uploads/storage_service";
-    let file = blob;
-    let storageServiceRes, presigned_url, download_url;
-
-    if (!(file instanceof File) || !(file instanceof Blob)) {
-      file = blob.blob();
-    }
+    let file =
+      blob instanceof File || blob instanceof Blob ? blob : blob.blob();
 
     try {
-      storageServiceRes = await fetch(storageServiceUrl, {
-        method: "GET",
-      });
-
-      if (storageServiceRes.ok === false) {
-        throw `${storageServiceRes.status} - ${storageServiceRes.statusText} - while fetching the storage service URL.`;
-      }
-    } catch (err) {
-      failure({ message: err, remove: true });
-    }
-
-    try {
-      const urls = await storageServiceRes.json();
-      presigned_url = urls.presigned_url;
-      download_url = urls.download_url;
-    } catch (err) {
-      failure({ message: err, remove: true });
-    }
-
-    try {
-      const uploadRes = await fetch(presigned_url, {
-        method: "PUT",
-        body: file,
-      });
-
-      if (!uploadRes.ok) {
-        throw `${uploadRes.status} - ${uploadRes.statusText}`;
-      }
-
-      const event = new CustomEvent("imageUploadSuccess", {
-        detail: { downloadUrl: download_url },
-      });
-      document.dispatchEvent(event);
+      const response = await getStorageServiceUrl(storageServiceUrl);
+      const urls = await formatToJson(response);
+      const { presigned_url: presignedUrl, download_url: downloadUrl } = urls;
+      await uploadImage(presignedUrl, file);
+      dispatchImageUploadListener(downloadUrl);
 
       success(
         "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"
@@ -50,4 +18,70 @@ export function imagesUploadHandler(blob, _) {
       failure({ message: err, remove: true });
     }
   });
+}
+
+export function imagesUploadHandler(blob, _) {
+  return new Promise(async (success, failure) => {
+    const storageServiceUrl = "/uploads/storage_service";
+    let file =
+      blob instanceof File || blob instanceof Blob ? blob : blob.blob();
+
+    try {
+      const response = await getStorageServiceUrl(storageServiceUrl);
+      const urls = await formatToJson(response);
+      const { presigned_url, download_url } = urls;
+      await uploadImageTo(presigned_url, file);
+
+      success(download_url);
+    } catch (err) {
+      failure({ message: err, remove: true });
+    }
+  });
+}
+
+async function getStorageServiceUrl(url) {
+  try {
+    let response = await fetch(url, {
+      method: "GET",
+    });
+
+    if (response.ok === false) {
+      throw `${response.status} - ${response.statusText} - while fetching the storage service URL.`;
+    }
+    return response;
+  } catch (err) {
+    throw err;
+  }
+}
+
+async function formatToJson(response) {
+  try {
+    const urls = await response.json();
+    return urls;
+  } catch (err) {
+    throw err;
+  }
+}
+
+async function uploadImage(presignedUrl, file) {
+  try {
+    const response = await fetch(presignedUrl, {
+      method: "PUT",
+      body: file,
+    });
+
+    if (!response.ok) {
+      throw `${response.status} - ${response.statusText}`;
+    }
+    return response;
+  } catch (err) {
+    throw err;
+  }
+}
+
+function dispatchImageUploadListener(downloadUrl) {
+  const event = new CustomEvent("imageUploadSuccess", {
+    detail: { downloadUrl: downloadUrl },
+  });
+  document.dispatchEvent(event);
 }
