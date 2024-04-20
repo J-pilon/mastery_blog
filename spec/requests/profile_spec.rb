f94bcaee992df0c465ccca8bf5e34d9b5935ecd9
Josiah <josiah.pilon@gmail.com>
2024-04-19 23:25:29 -0700

    fix: profile request spec

--- spec/requests/profile_spec.rb
+++ spec/requests/profile_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'Profiles', type: :request do
-  let(:profile) { FactoryBot.create(:profile) }
+  let(:user) { FactoryBot.create(:user) }
 
   describe 'GET /profile/:id' do
     it 'returns http success' do
-      get profile_path(id: profile)
+      get profile_path(id: user.profile)
       expect(response).to have_http_status(200)
     end
   end
 
   describe 'GET profile/edit/:id' do
     it 'returns http success' do
-      get edit_profile_path(id: profile)
+      get edit_profile_path(id: user.profile)
       expect(response).to have_http_status(200)
     end
   end
@@ -21,7 +21,7 @@ RSpec.describe 'Profiles', type: :request do
     context 'when params are valid' do
       it 'returns http success' do
         profile_params = { profile: { first_name: 'fake', last_name: 'fake' } }
-        put profile_path(id: profile), params: profile_params
+        put profile_path(id: user.profile), params: profile_params
         expect(response).to have_http_status(302)
       end
     end
@@ -29,7 +29,7 @@ RSpec.describe 'Profiles', type: :request do
     context 'when params are invalid' do
       it 'returns http success' do
         profile_params = { profile: { first_name: nil, last_name: nil } }
-        put profile_path(id: profile), params: profile_params
+        put profile_path(id: user.profile), params: profile_params
         expect(response).to have_http_status(422)
       end
     end
